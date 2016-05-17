ce88f6adcc25c1b9a12a6d70bca09d4c411870a3
cherniy <sergeywebjob@gmail.com>
2016-05-17 20:04:24 +0000

    add bootstrup and jquery by bower and compile are by gulp

--- src/FormBundle/Controller/SmallController.php
+++ src/FormBundle/Controller/SmallController.php
@@ -2,16 +2,37 @@
 
 namespace FormBundle\Controller;
 
+use FormBundle\Form\CountryType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use FormBundle\Entity\Country;
 
 class SmallController extends Controller
 {
     /**
      * @Route("/small", name="form_small")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('FormBundle:Small:index.html.twig');
+        $form = $this->createForm(CountryType::class)->handleRequest($request);
+
+        if ($form->isValid()) {
+            $country = new Country();
+            $country->setName($form->get(CountryType::FIELD_NAME)->getData());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($country);
+            $em->flush();
+        }
+
+        return $this->render(
+            'FormBundle:Small:index.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
     }
 }
